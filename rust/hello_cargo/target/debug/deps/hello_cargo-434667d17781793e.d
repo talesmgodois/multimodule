2883c6dbc716de59796fd357442b92ecd0d24534
talesmgodois <tales.godois@gmail.com>
2023-09-27 21:39:30 -0300

    delete ignored files

--- rust/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e.d
+++
@@ -1,5 +0,0 @@
-/home/tales/projetos/personal/scripts/multimodule/rust/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e: src/main.rs
-
-/home/tales/projetos/personal/scripts/multimodule/rust/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e.d: src/main.rs
-
-src/main.rs:
--- rust/hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/tales/projetos/personal/scripts/multimodule/rust/hello_cargo/target/release/hello_cargo: /home/tales/projetos/personal/scripts/multimodule/rust/hello_cargo/src/main.rs
